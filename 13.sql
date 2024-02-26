698ffe0436a1986598846a7c16cc41fbad255338
Manasi Meghraj Haldankar <90810075+ManasiHaldankar@users.noreply.github.com>
2024-02-26 13:51:38 -0500

    Managers with at Least 5 Direct Reports

---
+++ 13.sql
@@ -0,0 +1,25 @@
+/* Question:
+Managers with at Least 5 Direct Reports
+
+Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+Write a solution to find managers with at least five direct reports.
+Return the result table in any order.
+
+
+#Solution:
+select e.name from Employee e join Employee e1
+on e.id = e1.managerId
+group by e1.managerId
+having count(e1.managerId)>=5;
