0249d152dc3f28b3ba3878b09a9a5f74f8f56676
Manasi Meghraj Haldankar <90810075+ManasiHaldankar@users.noreply.github.com>
2024-04-29 13:43:10 -0400

    Friend Requests II: Who Has the Most Friends

---
+++ 41.sql
@@ -0,0 +1,35 @@
+/* Question:
+Friend Requests II: Who Has the Most Friends
+
+Table: RequestAccepted
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| requester_id   | int     |
+| accepter_id    | int     |
+| accept_date    | date    |
++----------------+---------+
+(requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+ 
+Write a solution to find the people who have the most friends and the most friends number.
+
+The test cases are generated so that only one person has the most friends.
+Return the result table in any order.
+
+
+#Solution:
+with cte as (
+    select requester_id as id 
+    from RequestAccepted
+    union all
+    select accepter_id as id
+    from RequestAccepted
+)
+
+select id, count(*) as num
+from cte
+group by id
+order by num desc
+limit 1
+;
